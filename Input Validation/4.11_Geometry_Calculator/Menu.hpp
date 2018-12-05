f64c7f0bc6a4d23f6c7ca9a09bf3f0d5b99d2945
Andrew Wilson <aw-leigh@users.noreply.github.com>
2018-12-05 19:03:26 +0900

    Fixed description, removed redundant function headers
    I had forgotten to remove function headers for validateInt and validateDouble. These have been removed.

--- Input Validation/4.11_Geometry_Calculator/Menu.hpp
+++ Input Validation/4.11_Geometry_Calculator/Menu.hpp
@@ -1,7 +1,7 @@
 /******************************************************************************
  * ** Author:      Andrew Wilson
  * ** Date:        Dec. 5 2018
- * ** Description  Header file for custom input validation functions
+ * ** Description  Header file for menu display and choice validation function
  * ****************************************************************************/
 
 #include <iostream>
@@ -10,6 +10,4 @@ const int MAXCHOICE = 4;  //maximum menu choice number
 const int MINCHOICE = 1;  //minimum menu choice number
 void displayMenu();  //displays menu text
 int validateMenuChoice(int &choice);  //checks that the choice parameter is an integer between MAXCHOICE and MINCHOICE
-                                     //if it is, returns choice; if not, continually reprompts until it is, then returns that value
-int validateInt(int &choice);  //ensures that input is an integer
-double validateDouble(double &choice);  //ensures that input is a double                                  
+                                     //if it is, returns choice; if not, continually reprompts until it is, then returns that value                                 
