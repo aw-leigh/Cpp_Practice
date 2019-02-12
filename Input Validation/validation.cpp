8e5af8d957579c194a10109953317d5e45f8906f
aw-leigh <puddingisfun@gmail.com>
2019-02-12 13:09:08 +0900

    included new overloaded validation functions

--- Input Validation/validation.cpp
+++ Input Validation/validation.cpp
@@ -13,7 +13,7 @@ void validateInt(int &input)
 		std::cin >> input;
 	}
 }
-void validateRangedInt(int &input, int min, int max)
+void validateInt(int &input, int min, int max)
 {
    	while (!std::cin || std::cin.peek() == '.' || std::cin.peek() == ' ' || 
 	   		std::isalpha(std::cin.peek()) || input < min || input > max)
@@ -23,6 +23,59 @@ void validateRangedInt(int &input, int min, int max)
 		std::cout << "Input must be an integer between " << min << " and " << max << std::endl;
 		std::cin >> input;
 	} 
+}
+int validateInt()
+{
+	int input;
+	std::cin >> input;
+	while (!std::cin || std::cin.peek() == '.' || std::cin.peek() == ' ' || std::isalpha(std::cin.peek()))
+	{
+		std::cin.clear();  //Clear bad input flag
+		std::cin.ignore(1000, '\n');  //Discard input
+		std::cout << "Input must be an integer" << std::endl;
+		std::cin >> input;
+	}
+	return input;
+}
+int validateInt(int min, int max)
+{
+   	int input;
+	std::cin >> input;
+	while (!std::cin || std::cin.peek() == '.' || std::cin.peek() == ' ' || 
+	   		std::isalpha(std::cin.peek()) || input < min || input > max)
+	{
+		std::cin.clear();  //Clear bad input flag
+		std::cin.ignore(1000, '\n');  //Discard input
+		std::cout << "Input must be an integer between " << min << " and " << max << std::endl;
+		std::cin >> input;
+	} 
+	return input;
+}
+double validateDouble()
+{
+   	double input;
+	std::cin >> input;
+    while (!std::cin || std::cin.peek() == ' ' || std::isalpha(std::cin.peek()))
+    {
+        std::cin.clear();
+        std::cin.ignore(10000,'\n');
+        std::cout << "Input must be a decimal" << std::endl;
+        std::cin >> input;
+    }
+	return input;
+}
+double validateDouble(double min, double max)
+{
+   	double input;
+	std::cin >> input;
+    while (!std::cin || std::cin.peek() == ' ' || std::isalpha(std::cin.peek()) || input < min || input > max)
+    {
+        std::cin.clear();
+        std::cin.ignore(10000,'\n');
+        std::cout << "Input must be a decimal between " << min << " and " << max << std::endl;
+        std::cin >> input;
+    }
+	return input;
 }
 void validateDouble(double &input)
 {
@@ -34,7 +87,7 @@ void validateDouble(double &input)
         std::cin >> input;
     }
 }
-void validateRangedDouble(double &input, double min, double max)
+void validateDouble(double &input, double min, double max)
 {
     while (!std::cin || input < min || input > max)
     {
@@ -54,4 +107,37 @@ void validateMenuChoice(int &menuChoice, int menuLength)  //assumes menu choices
 		std::cout << "Please enter a number between 1 and " << menuLength << std::endl;
 		std::cin >> menuChoice;
 	}
+}
+void clearScreen()
+{
+	#ifdef _WIN32
+		std::system("cls");
+	#else
+		//Assume POSIX
+		std::cout << "\033[2J\033[1;1H";
+	#endif
+}
+
+void systemPause()
+{
+	std::cin.clear();
+	std::cin.ignore(1024, '\n');  // Discart old input
+	std::cout << "Press enter to continue...";
+	std::cin.get();  // Proceed after new input from user
+}
+
+std::string getValidatedString()
+{
+	std::cin.clear();
+	std::cin.ignore(1024, '\n');  // Discart old input
+
+	std::string inputString;
+
+	while(!std::getline(std::cin, inputString))
+	{
+		std::cout << "Error, please reenter string:" << std::endl;
+		std::cin.clear();	
+		std::cin.ignore(1024, '\n');
+	}
+	return inputString;
 }
--- Input Validation/validation.hpp
+++ Input Validation/validation.hpp
@@ -7,11 +7,25 @@
 #ifndef VALIDATION_HPP
 #define VALIDATION_HPP
 
-void validateInt(int &input);
-void validateRangedInt(int &input, int min, int max);
+//takes int by reference, reprompts if not an integer
+void validateInt(int &input);  
+//takes int by reference, and int fir min and max. Reprompts if input is not an integer or between min and max
+void validateInt(int &input, int min, int max);  
+//returns an int. Prompts user for integer, reprompting if not an int
+int validateInt();  
+//takes ints for min and max, returns an int. Prompts user for integer, reprompting if not an int/between min-max
+int validateInt(int min, int max);
+//takes double input by reference, reprompts if input is not a decimal/integer
 void validateDouble(double &input);
-void validateRangedDouble(double &input, double min, double max);
-void validateMenuChoice(int &menuChoice, int menuLength);
-int validateInt2(std::string &input);
+//takes double input by reference and doubles for min, max. Reprompts if input is not a decimal or between min and max
+void validateDouble(double &input, double min, double max); 
+//returns a double. Prompts user for double, reprompting if not a decimal/integer
+double validateDouble();
+//takes doubles for min and max, returns a double. Reprompts if not a decimal/integer between min-max
+double validateDouble(double min, double max);
+//clears the screen
+void clearScreen();  
+//Pauses until user presses a key
+void systemPause();  
 
-#endif
+#endif 
